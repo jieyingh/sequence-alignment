0908390ecec9f3532516e448e994c6f2f24a137f
Jie Ying Huang <124211458+jieyingh@users.noreply.github.com>
2024-01-31 13:09:07 -0500

    backtracking
    now finds the path (or one of them) that leads to the end and is able to display it wow

--- alignment.py
+++ alignment.py
@@ -1,10 +1,17 @@
 import numpy as np 
 import pandas as pd 
 
+paths = []
+
 def alignement(seq1, seq2, scoreGap, scoreMatch, scoreMismatch):
     (matrice, numRow, numCol, rowSeq, colSeq) = createMatrice(seq1, seq2)
     matrice = firstScore(matrice,numCol, numRow, scoreGap)
-    calcScore(matrice, numRow, numCol, scoreGap, scoreMatch, scoreMismatch, rowSeq, colSeq)
+    matrice = calcScore(matrice, numRow, numCol, scoreGap, scoreMatch, scoreMismatch, rowSeq, colSeq)
+    chemin = backtrack(numRow, numCol)
+    visAlignement(chemin)
+    print(chemin)
+    score(matrice, numRow, numCol)
+
 
 # cree le dataframe
 def createMatrice(seq1, seq2):
@@ -32,22 +39,70 @@ def calcScore(matrice, numRow, numCol, scoreGap, scoreMatch, scoreMismatch, rowS
     for row in range(1, numRow):
         for col in range(1, numCol):
             # matches, takes top left and adds scoreMatch
+
             if colSeq[col] == rowSeq[row]:
                 matrice.iat[row,col] = matrice.iat[(row - 1),(col - 1)] + scoreMatch
+                match(row, col)
             else:
-                pass
-    print(matrice)
+                mismatch = matrice.iat[(row - 1),(col - 1)] + scoreMismatch
+                gapSeq1 = matrice.iat[(row),(col - 1)] + scoreGap
+                gapSeq2 = matrice.iat[(row - 1),(col)] + scoreGap
+                minVal = matrice.iat[row,col] = min(mismatch,gapSeq1,gapSeq2)
+                notMatch(row, col, minVal, mismatch, gapSeq1, gapSeq2)
+
+    return matrice
+
+# ajout du chemin si match
+def match(row, col):
+    pathTrack((row,col), ((row - 1),(col - 1)), "match")
+
+# ajout du chemin si pas de match
+def notMatch(row, col, minVal, mismatch, gapSeq1, gapSeq2):
+    if mismatch == minVal:
+        pathTrack((row,col), ((row - 1),(col - 1)), "mismatch")
+    if gapSeq1 == minVal:
+        pathTrack((row,col), (row,(col - 1)), "gapSeq1")
+    elif gapSeq2 == minVal:
+        pathTrack((row,col), ((row - 1),col), "gapSeq2")
+
+# log des chemins pris
+def pathTrack(current, previous, type):
+    dict = {
+        "current": current,
+        "previous": previous,
+        "type": type}
+    paths.append(dict)
 
 
 # extrait le score final de la matrice
-def score(matrice):
-    pass
+def score(matrice, numRow, numCol):
+    print(matrice)
+    print("Score de l'alignement: ", matrice.iat[(numRow - 1),(numCol - 1)])
+    
 
 # donne l'alignement
-def visAlignement(matrice):
-    # will return a list: top strand, bottom strand
+def visAlignement(chemin):
     pass
 
+def backtrack(numRow, numCol):
+    chemin = []
+    fin = ((numRow - 1), (numCol -1))
+
+    target = fin
+    search = True
+
+    while search:
+        found = False
+        for dict in paths:
+            if dict.get("current") == target:
+                chemin.append(dict)
+                target = dict.get("previous") # sets new target
+                found = True
+                break
+        if not found:
+            search = False # stops the loop if no more matches are found
+
+    return chemin
 
 # ================= display ==========================
 seq1 = input(" Entrez la premiere sequence: ")
