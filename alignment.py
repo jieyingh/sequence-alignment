85b806a55d173e1dff69086dbb486b5c956f20a8
Jie Ying Huang <124211458+jieyingh@users.noreply.github.com>
2024-01-30 22:56:50 -0500

    match score
    now able to calculate matches! but only that

--- alignment.py
+++ alignment.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd 
 
 def alignement(seq1, seq2, scoreGap, scoreMatch, scoreMismatch):
-    (matrice, numRow, numCol) = createMatrice(seq1, seq2)
-    calcScore(matrice, numRow, numCol, int(scoreGap), int(scoreMatch), int(scoreMismatch))
+    (matrice, numRow, numCol, rowSeq, colSeq) = createMatrice(seq1, seq2)
+    matrice = firstScore(matrice,numCol, numRow, scoreGap)
+    calcScore(matrice, numRow, numCol, scoreGap, scoreMatch, scoreMismatch, rowSeq, colSeq)
 
 # cree le dataframe
 def createMatrice(seq1, seq2):
@@ -15,22 +16,28 @@ def createMatrice(seq1, seq2):
 
     matrice = pd.DataFrame(0, index=rowSeq, columns=colSeq)
     
-    return (matrice, numRow, numCol)
+    return (matrice, numRow, numCol, rowSeq, colSeq)
 
-# calcule le score de chaque case
-def calcScore(matrice, numRow, numCol, scoreGap, scoreMatch, scoreMismatch):
-    # calcul de la premiere colonne et rangee
+# calcul de la premiere colonne et rangee
+def firstScore(matrice, numCol, numRow, scoreGap):
     for col in range(1,numCol):
         matrice.iat[0,col] = matrice.iat[0,(col -1)] + scoreGap
     for row in range(1,numRow):
         matrice.iat[row,0] = matrice.iat[(row -1),0] + scoreGap
+    
+    return matrice   
 
-    # calcul des autres scores
+# calcule le score de chaque case
+def calcScore(matrice, numRow, numCol, scoreGap, scoreMatch, scoreMismatch, rowSeq, colSeq):
     for row in range(1, numRow):
         for col in range(1, numCol):
-            matrice.iat[row,col] = col
+            # matches, takes top left and adds scoreMatch
+            if colSeq[col] == rowSeq[row]:
+                matrice.iat[row,col] = matrice.iat[(row - 1),(col - 1)] + scoreMatch
+            else:
+                pass
+    print(matrice)
 
-    print(matrice, numRow, numCol, scoreGap, scoreMatch, scoreMismatch)
 
 # extrait le score final de la matrice
 def score(matrice):
@@ -49,9 +56,9 @@ defaut = None
 while defaut != "Y" and defaut != "N":
     defaut = input(" Utiliser les scores par defaut: scoreGap = 2, scoreMatch = 0, scoreMismatch = 1? Y/N ")
     if defaut == "N":
-        scoreGap = input(" Entrez le scoreGap: ")
-        scoreMatch = input(" Entrez le scoreMatch: ")
-        scoreMismatch = input(" Entrez le scoreMismatch: ")
+        scoreGap = int(input(" Entrez le scoreGap: "))
+        scoreMatch = int(input(" Entrez le scoreMatch: "))
+        scoreMismatch = int(input(" Entrez le scoreMismatch: "))
     elif defaut == "Y":
         scoreGap = 2
         scoreMatch = 0
